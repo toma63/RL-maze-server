7d61b0cc38ab13dd6f3c472a6b8fb7ec81ecf8d2
Tom Arns <tomrarns@gmail.com>
2024-07-16 13:27:06 -0700

    add post request to run training in the server

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def create_maze():
     data = request.get_json()
     if not data:
         abort(400)
-    maze = Maze(data)
+    maze = Maze(data) # rows and cols
     maze.make_maze(MazeCell(0, 0, maze))
     return jsonify(maze.dict_for_json()), 201
 
@@ -27,6 +27,15 @@ def get_maze():
         abort(400)
     return jsonify(maze.dict_for_json())
 
+# Run training
+@app.route('/train', methods=['POST'])
+def run_q_learning():
+    global maze
+    data = request.get_json()
+    if not data:
+        abort(400)
+    maze.rl_train(data['passes']) # number of passes to run
+    return jsonify({'total_training_passes': maze.total_training_passes}), 200 # returns total training passes
 
 if __name__ == '__main__':
     app.run(debug=True)
